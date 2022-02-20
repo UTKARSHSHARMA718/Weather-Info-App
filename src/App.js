98c2b03c57ef30cc419442c34582bfe63c63758c
UTKARSH SHARMA <86897568+UTKARSHSHARMA718@users.noreply.github.com>
2022-02-20 23:54:18 +0530

    1st commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Wheather from './componenets/Wheather';
+
+function App() {
+  return (
+    <div className="container">
+      <Wheather/>
+    </div>
+  );
+}
+
+export default App;
